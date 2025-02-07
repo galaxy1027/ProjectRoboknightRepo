1ec7876c53c96688aea0cf43828cd796645782e4
Camden Perkins <camden@live.com>
2025-02-07 15:18:06 -0800

    LEVEL 2

--- Levels/intro.gd
+++ Levels/intro.gd
@@ -6,7 +6,7 @@ func _ready() -> void:
 
 func _on_button_pressed() -> void:
 	# Start main scene
-	get_tree().change_scene_to_file("res://Levels/test_level.tscn")
+	get_tree().change_scene_to_file("res://Levels/LEVEL1.tscn")
 	pass # Replace with function body.
 
 
